8516fda6ed1e0bef44a1ba0791b1ac1688b156f0
KotyshevaYulya <92879736+KotyshevaYulya@users.noreply.github.com>
2024-03-12 16:26:43 +0100

    update

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,21 +1,10 @@
 "use strict";
 const STORAGE_KEY = "feedback-form-state";
 const form = document.querySelector(".feedback-form");
-const textarea = document.querySelector("form textarea[name=message]");
+const textarea = document.querySelector("textarea");
+
 
 form.addEventListener("input", formSubmitHandler)
-
-form.addEventListener("submit", (event) => {
-    if (textarea.value === '' || form.elements.email.value === '') {
-        return;
-    } else {
-        event.preventDefault();
-        textarea.value = "";
-        form.elements.email.value = '';
-        localStorage.removeItem(STORAGE_KEY);
-        console.log(getValue);
-    }
-});
 
 function formSubmitHandler(event) {
     event.preventDefault();
@@ -24,13 +13,26 @@ function formSubmitHandler(event) {
 
     const saveValue = JSON.stringify({ message, email });
     localStorage.setItem(STORAGE_KEY, saveValue);
-    
 }; 
 
 const jsn = localStorage.getItem(STORAGE_KEY);
 const getValue = JSON.parse(jsn);
-    
+
 if (jsn !== null) {
     textarea.value = getValue.message;
     form.elements.email.value = getValue.email;
 };
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (textarea.value === '' || form.elements.email.value === '') {
+        return;
+    } else {
+        form.reset();
+        console.log(getValue);
+        localStorage.removeItem(STORAGE_KEY);
+    }
+});
+
+
+
