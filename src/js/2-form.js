4aed22901da40e998f3d82fc0cb75d1e15e3122f
KotyshevaYulya <92879736+KotyshevaYulya@users.noreply.github.com>
2024-03-12 17:46:07 +0100

    update

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,37 +2,50 @@
 const STORAGE_KEY = "feedback-form-state";
 const form = document.querySelector(".feedback-form");
 const textarea = document.querySelector("textarea");
+let parsedData;
 
+form.addEventListener("input", formInputHandler);
 
-form.addEventListener("input", formSubmitHandler)
-
-function formSubmitHandler(event) {
+function formInputHandler(event) {
     event.preventDefault();
     const message = textarea.value;
     const email = form.elements.email.value;
-
     const saveValue = JSON.stringify({ message, email });
     localStorage.setItem(STORAGE_KEY, saveValue);
-}; 
-
-const jsn = localStorage.getItem(STORAGE_KEY);
-const getValue = JSON.parse(jsn);
-
-if (jsn !== null) {
-    textarea.value = getValue.message;
-    form.elements.email.value = getValue.email;
 };
+
+const storedData = localStorage.getItem(STORAGE_KEY);
+
+if (storedData !== null) {
+    parsedData = JSON.parse(storedData);
+    textarea.value = parsedData.message;
+    form.elements.email.value = parsedData.email;
+}
+
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    if (textarea.value === '' || form.elements.email.value === '') {
-        return;
+    if (textarea.value === "" || form.elements.email.value === "") {
+        return alert("Заповніть усі поля");
     } else {
         form.reset();
-        console.log(getValue);
+        console.log(parsedData);
         localStorage.removeItem(STORAGE_KEY);
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
